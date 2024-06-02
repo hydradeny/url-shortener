354d1a96af6d2793d30413968778f41c3ea7bea9
Vlad <vlad@examle.com>
2024-06-02 12:43:36 +0300

    fix auth middleware signature

--- auth_service/internal/transport/http/midleware/auth_middleware.go
+++ auth_service/internal/transport/http/midleware/auth_middleware.go
@@ -8,24 +8,26 @@ import (
 	"github.com/hydradeny/url-shortener/auth_service/internal/transport/http/handlers/authhandler"
 )
 
-func AuthMiddleware(sm session.SessionManager, next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
-		sessionCookie, err := r.Cookie(authhandler.CookieName)
-		if err == http.ErrNoCookie {
-			w.WriteHeader(http.StatusUnauthorized)
-			return
-		}
-		checkIn := &session.CheckSession{
-			SessionID: sessionCookie.Value,
-		}
+func NewAuthMiddleware(sm *session.SessionManager) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ctx := r.Context()
+			sessionCookie, err := r.Cookie(authhandler.CookieName)
+			if err == http.ErrNoCookie {
+				w.WriteHeader(http.StatusUnauthorized)
+				return
+			}
+			checkIn := &session.CheckSession{
+				SessionID: sessionCookie.Value,
+			}
 
-		sess, err := sm.Check(ctx, checkIn)
-		if err != nil {
-			http.Error(w, "No auth", http.StatusUnauthorized)
-			return
-		}
-		ctx = context.WithValue(ctx, authhandler.CtxSessionKey, sess)
-		next.ServeHTTP(w, r.WithContext(ctx))
-	})
+			sess, err := sm.Check(ctx, checkIn)
+			if err != nil {
+				http.Error(w, "No auth", http.StatusUnauthorized)
+				return
+			}
+			ctx = context.WithValue(ctx, authhandler.CtxSessionKey, sess)
+			next.ServeHTTP(w, r.WithContext(ctx))
+		})
+	}
 }

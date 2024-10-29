6a029f272e094002076e97a5663b23346d9b5871
Vlad <vlad@examle.com>
2024-10-29 18:04:14 +0300

    fix db migration

--- auth_service/migrations/1_init.up.sql
+++ auth_service/migrations/1_init.up.sql
@@ -4,14 +4,14 @@ CREATE TABLE IF NOT EXISTS users
 (
     id           SERIAL	 PRIMARY KEY,
     email        TEXT    NOT NULL UNIQUE,
-    pass_hash    BYTEA    NOT NULL
+    password    BYTEA    NOT NULL
 );
 CREATE INDEX IF NOT EXISTS idx_email ON users (email);
 
 CREATE TABLE IF NOT EXISTS sessions
 (
     id     TEXT NOT NULL UNIQUE,
-    user_id   TEXT NOT NULL UNIQUE
+    user_id   BIGINT NOT NULL
 );
 
 COMMIT;
